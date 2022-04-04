ba99a588a56213064e0c575c14530b243cefb1f7
Kangmas Darr <92195557+Darrma23@users.noreply.github.com>
2022-04-04 09:41:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,12 @@ global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse()
 // console.log({ opts })
 global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
-global.db = new Low(new mongoDB('mongodb+srv://kangmasDarr:anakayamm2@cluster0.y8y23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
+global.db = new Low(
+  /https?:\/\//.test(opts['db'] || '') ?
+    new cloudDBAdapter(opts['db']) : /mongodb(\+srv)?:\/\//i.test(opts['db']) ?
+      (opts['mongodbv2'] ? new mongoDBV2(opts['db']) : new mongoDB(opts['db'])) :
+      new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`)
+)
 
 global.DATABASE = global.db // Backwards Compatibility
 global.loadDatabase = async function loadDatabase() {
